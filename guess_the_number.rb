87c7301ca5da698d6efe935eacb57d156c100dd4
rasleson02@yahoo.com <Rasleson02>
2014-03-18 18:06:12 +0000

    checked guess and secret number and returnd a message

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -1,3 +1,14 @@
 puts "welcome to guess the number !!!"
-secret_number = rand(6)
-puts secret_number
+secret_number = rand(100)
+puts "guess a number between 1 and 99"
+guess = gets.chomp.to_i
+
+
+if secret_number == guess  
+  puts "congrats u got it right :]"
+  
+else
+  puts "wrong! #{secret_number} fail"
+  
+  
+end
