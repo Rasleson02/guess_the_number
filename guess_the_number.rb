9c7daaf2d00638de9bdb0bd5c8e9afb0a6f47326
rasleson02@yahoo.com <Rasleson02>
2014-03-20 17:55:46 +0000

    I tried to have the code end when the user hits 3 tries

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -1,10 +1,20 @@
 puts "welcome to guess the number !!!"
-secret_number = rand(100)
-puts "guess a number between 1 and 99"
-guess = gets.chomp.to_i
+secret_number = rand(10)
 
-until guess==secret_number
-  puts "wrong! fail"
-   guess = gets.chomp.to_i
+puts "guess a number between 1 and 9"
+guess = gets.chomp.to_i
+tries = 0
+ until guess == secret_number
+  puts "wrong! fail,try angin"
+    
+guess = gets.chomp.to_i
+if tries < 5
+
 end
-puts "congrats! you won:]"
+end
+gir
+
+puts "congrats! you won:]                                                                                                                                                                                               
+  / |/ /  _/_  __/ _ \/ __ \/ / / / __/ /  _/ __ \                                                                                                                                                                                             
+ /    // /  / / / , _/ /_/ / /_/ /\ \_ _/ // /_/ /                                                                                                                                                                                             
+/_/|_/___/ /_/ /_/|_|\____/\____/___(_)___/\____/     "
